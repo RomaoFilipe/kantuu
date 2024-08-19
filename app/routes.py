2a1f7c35886988a0a1fb0950ef991df460302989
Romao Filipe <Aluno20923@ipt.pt>
2024-08-19 10:19:31 +0100

    version 4

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,7 @@ def fetch_google_reviews():
     if time.time() - last_updated > 60:  # Atualiza a cada 24 horas
         api_key = 'AIzaSyBOFMxpjGme0_rNF70lMLmbNBtxtkOngvE'
         place_id = '0xd19e57fc71ff81f:0xb647930f79b4e4f9'
-        url = f'https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&fields=reviews&key={api_key}'
+        url = 'https://maps.googleapis.com/maps/api/place/details/json?place_id={}&fields=reviews&key={}'.format(place_id, api_key)
         response = requests.get(url)
         cached_reviews = response.json().get('result', {}).get('reviews', [])
         last_updated = time.time()
